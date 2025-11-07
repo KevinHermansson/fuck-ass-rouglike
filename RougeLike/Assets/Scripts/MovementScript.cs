f122adbdd9a5c94f56a3e253cb0cf3927cc5631b
isakcarlsson25 <isak.carlsson@elev.ntig.se>
2025-11-07 12:25:29 +0100

    sprite added

--- RougeLike/Assets/Scripts/MovementScript.cs
+++ RougeLike/Assets/Scripts/MovementScript.cs
@@ -13,6 +13,8 @@ public class MovementScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        
 
     }
 
