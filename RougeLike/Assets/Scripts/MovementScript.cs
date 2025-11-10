647bb792937cbbaa2893669ff8354514306e1e67
KevinHermansson <163125550+KevinHermansson@users.noreply.github.com>
2025-11-10 11:36:56 +0100

    Added oneway platforms

--- RougeLike/Assets/Scripts/MovementScript.cs
+++ RougeLike/Assets/Scripts/MovementScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class MovementScript : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class MovementScript : MonoBehaviour
 
 
     public float groundCheck = 1;
+    public bool fancyGroundCheck;
+    BoxCollider2D PlayerCollider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        PlayerCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -28,11 +31,14 @@ public class MovementScript : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        } if (Input.GetKey(KeyCode.S)){
+            dropThroughPlatform();
         }
         else if (Input.GetKey(KeyCode.A))
         {
             rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
         }
+
 
     }
 
@@ -41,11 +47,28 @@ public class MovementScript : MonoBehaviour
     {
         if (collision.collider.CompareTag("Ground"))
             groundCheck = 1;
+        if (collision.collider.CompareTag("fancyPlatform"))
+            fancyGroundCheck = true;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
             groundCheck = 0;
+        if (collision.collider.CompareTag("fancyPlatform"))
+            fancyGroundCheck = false;
+    }
+
+    private IEnumerator DisablePlayerCollider(float disableTime){
+        PlayerCollider.enabled = false;
+        yield return new WaitForSeconds(disableTime);
+        PlayerCollider.enabled = true;
+    }
+
+    public void dropThroughPlatform()
+    {
+        if (Input.GetKey(KeyCode.S) && fancyGroundCheck && PlayerCollider.enabled){
+            StartCoroutine(DisablePlayerCollider(0.5f));
+        }
     }
 }
