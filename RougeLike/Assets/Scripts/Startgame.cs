bdcf1f7ee71f0680ce13f5c5123589de63db772b
IsakPetersson <isak.petersson@elev.ga.ntig.se>
2025-11-24 14:24:47 +0100

    Hub with base for start mechanic

---
+++ RougeLike/Assets/Scripts/Startgame.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Startgame : MonoBehaviour
+{
+    public GameObject startGamePanel;
+    void Update()
+    {
+        if (playerInRange2 && Input.GetKeyDown(KeyCode.E))
+        {
+            if (startGamePanel.activeInHierarchy)
+            {
+                startGamePanel.SetActive(false);
+            }
+            else
+                startGamePanel.SetActive(true);
+            Debug.Log("Start Game Triggered");
+        }
+    }
+
+    public bool playerInRange2;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange2 = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange2 = false;
+            startGamePanel.SetActive(false);
+        }
+    }
+}
